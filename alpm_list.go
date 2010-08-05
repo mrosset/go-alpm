8a2be551428599e37db9236deec34e738b4fd74a
Dave Reisner <d@falconindy.com>
2010-08-05 09:26:05 -0400

    list: use pointers as the object reference

--- alpm_list.go
+++ alpm_list.go
@@ -14,10 +14,10 @@ type AlpmList struct {
   alpm_list_t *C.alpm_list_t
 }
 
-func (v AlpmList) Free() {
+func (v *AlpmList) Free() {
   C.alpm_list_free(v.alpm_list_t)
 }
 
-func (v AlpmList) Add(data unsafe.Pointer) *AlpmList {
+func (v *AlpmList) Add(data unsafe.Pointer) *AlpmList {
   return &AlpmList{C.alpm_list_add(v.alpm_list_t, data)}
 }
