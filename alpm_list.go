d5668b39d63a21b075ef0d30ce68a208ea02dd3a
Dave Reisner <d@falconindy.com>
2010-08-06 21:32:30 -0400

    checkpoint

--- alpm_list.go
+++ alpm_list.go
@@ -11,57 +11,57 @@ import (
 )
 
 type AlpmList struct {
-  alpm_list_t *C.alpm_list_t
+  Alpm_list_t *C.alpm_list_t
 }
 
 /* allocation */
 func (v *AlpmList) Free() {
-  C.alpm_list_free(v.alpm_list_t)
+  C.alpm_list_free(v.Alpm_list_t)
 }
 
 /* mutators */
 func (v *AlpmList) Add(data interface{}) {
-  v = &AlpmList{C.alpm_list_add(v.alpm_list_t, unsafe.New(data))}
+  v.Alpm_list_t = C.alpm_list_add(v.Alpm_list_t, unsafe.New(data))
 }
 
 func (v *AlpmList) AddSorted(data interface{}, callback *[0]uint8) {
-  v = &AlpmList{C.alpm_list_add_sorted(v.alpm_list_t, unsafe.New(data), callback)}
+  v = &AlpmList{C.alpm_list_add_sorted(v.Alpm_list_t, unsafe.New(data), callback)}
 }
 
 func (v *AlpmList) Join(other *C.alpm_list_t) {
-  v = &AlpmList{C.alpm_list_join(v.alpm_list_t, other)}
+  v = &AlpmList{C.alpm_list_join(v.Alpm_list_t, other)}
 }
 
 /* accessors */
 func (v *AlpmList) First() *AlpmList {
-  return &AlpmList{C.alpm_list_first(v.alpm_list_t)}
+  return &AlpmList{C.alpm_list_first(v.Alpm_list_t)}
 }
 
 func (v *AlpmList) Nth(n C.int) *AlpmList {
-  return &AlpmList{C.alpm_list_nth(v.alpm_list_t, n)}
+  return &AlpmList{C.alpm_list_nth(v.Alpm_list_t, n)}
 }
 
 func (v *AlpmList) Next() *AlpmList {
-  return &AlpmList{C.alpm_list_next(v.alpm_list_t)}
+  return &AlpmList{C.alpm_list_next(v.Alpm_list_t)}
 }
 
 func (v *AlpmList) Last() *AlpmList {
-  return &AlpmList{C.alpm_list_last(v.alpm_list_t)}
+  return &AlpmList{C.alpm_list_last(v.Alpm_list_t)}
 }
 
-func (v *AlpmList) GetData() interface{} {
-  return v.alpm_list_t.data
+func (v *AlpmList) GetData() unsafe.Pointer {
+  return v.Alpm_list_t.data
 }
 
 /* misc */
 func (v *AlpmList) Count() uint {
-  return uint(C.alpm_list_count(v.alpm_list_t))
+  return uint(C.alpm_list_count(v.Alpm_list_t))
 }
 
 func (v *AlpmList) FindStr(needle *C.char) string {
-  return C.GoString(C.alpm_list_find_str(v.alpm_list_t, needle))
+  return C.GoString(C.alpm_list_find_str(v.Alpm_list_t, needle))
 }
 
 func (v *AlpmList) FindPtr(needle interface{}) interface{} {
-  return C.alpm_list_find_ptr(v.alpm_list_t, unsafe.New(needle))
+  return C.alpm_list_find_ptr(v.Alpm_list_t, unsafe.New(needle))
 }
