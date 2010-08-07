8710e40f7bad5825de4f506a11330a42ae8f98b5
str1ngs <schizoid29@gmail.com>
2010-08-07 04:21:14 -0700

    Release, Init test scaffolding
    build is now smarter but not by much

--- alpm.go
+++ alpm.go
@@ -8,11 +8,32 @@ package alpm
 */
 import "C"
 
+import (
+  "os"
+)
 
-func Initialize() {
-  C.alpm_initialize()
+// Initializes libalpm
+func Initialize() os.Error {
+  if C.alpm_initialize() != 0 {
+    return lastError()
+  }
+  return nil
 }
 
+// Release libalpm
+func Release() os.Error {
+  if C.alpm_release() != 0 {
+    return lastError()
+  }
+  return nil
+}
+
+// Returns libalpm version
 func Version() string {
   return C.GoString(C.alpm_version())
+}
+
+// Get the last pm_error
+func lastError() os.Error {
+  return os.NewError(C.GoString(C.alpm_strerrorlast()))
 }
--- alpm_test.go
+++ alpm_test.go
@@ -1,6 +1,23 @@
 package alpm
 
 import "testing"
+import "fmt"
+
+func TestRelease(t *testing.T) {
+  Initialize()
+  err := Release()
+  if err != nil {
+    t.Fail()
+  }
+}
+
+func TestInitialize(t *testing.T) {
+  err := Initialize()
+  defer Release()
+  if err != nil {
+    t.Fail()
+  }
+}
 
 func TestVersion(t *testing.T) {
   if Version() != "5.0.0" {
