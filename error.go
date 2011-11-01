8c90dd933ec3019496a9f86d5d762249a9aab956
Rémy Oudompheng <remy@archlinux.org>
2011-11-01 10:22:34 +0100

    Describe what is Error type.

--- error.go
+++ error.go
@@ -3,8 +3,11 @@ package alpm
 // #include <alpm.h>
 import "C"
 
+// The Error type represents error codes from libalpm.
 type Error uint32
 
+// The string representation of an error is given by C function
+// alpm_strerror().
 func (er Error) String() string {
 	return C.GoString(C.alpm_strerror(uint32(er)))
 }
